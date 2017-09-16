fd3e43ee0d6ded7a2b7d606c77aa2b1514a8b540
Adrian Irving-Beer <wisq@wisq.net>
2017-09-16 15:38:08 -0400

    Initial `mix new`

---
+++ lib/plextube.ex
@@ -0,0 +1,18 @@
+defmodule Plextube do
+  @moduledoc """
+  Documentation for Plextube.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Plextube.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/plextube_test.exs
@@ -0,0 +1,8 @@
+defmodule PlextubeTest do
+  use ExUnit.Case
+  doctest Plextube
+
+  test "greets the world" do
+    assert Plextube.hello() == :world
+  end
+end
